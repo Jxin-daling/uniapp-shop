e54b37b615b0b5f3be9f57b4030270a4bfff958b
Jxin_darling <3167044821@qq.com>
2024-05-05 13:00:58 +0800

    首页类型文件

--- src/types/home.d.ts
+++ src/types/home.d.ts
@@ -9,3 +9,24 @@ export type BannerItem = {
     /** 跳转类型 */
     type: number
 }
+
+export type CategoryItem = {
+    icon: string
+    id: number,
+    name: string
+}
+/** 首页-热门推荐数据类型 */
+export type HotItem = {
+    /** 说明 */
+    alt: string
+    /** id */
+    id: string
+    /** 图片集合[ 图片路径 ] */
+    pictures: string[]
+    /** 跳转地址 */
+    target: string
+    /** 标题 */
+    title: string
+    /** 推荐类型 */
+    type: string
+}
