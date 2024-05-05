33645469d71e055ad3686559769a6eea3a07d6c1
Jxin_darling <3167044821@qq.com>
2024-05-05 13:25:39 +0800

    猜你喜欢数据获取请求API接口

--- src/services/home.ts
+++ src/services/home.ts
@@ -1,5 +1,5 @@
 import { http } from "@/utils/https"
-import type { BannerItem, CategoryItem,HotItem } from "@/types/home"
+import type { BannerItem, CategoryItem, HotItem } from "@/types/home"
 
 export const getHomeBannerAPI = (distributionSite = 1) => {
     return http<BannerItem[]>({
@@ -22,7 +22,18 @@ export const getHomeCategoryAPI = () => {
 //首页热门推荐接口
 export const getHomeHotAPI = () => {
     return http<HotItem[]>({
-        method:"GET",
-        url:'/home/hot/mutli'
+        method: "GET",
+        url: '/home/hot/mutli'
+    })
+}
+
+//首页猜你喜欢接口
+export const getHomeGuessLikeAPI = (page = 1, pageSize = 10) => {
+    return http({
+        method: "GET",
+        url: '/home/goods/guessLike',
+        data: {
+            page, pageSize
+        }
     })
 }
