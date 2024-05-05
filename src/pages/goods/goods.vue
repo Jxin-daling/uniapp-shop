e8a7c69adc0d2d996f18496b4e71eb83ebd42d73
Jxin_darling <3167044821@qq.com>
2024-05-06 00:34:26 +0800

    轮播图的交互和点击图片放大

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -17,6 +17,20 @@ const getGoodsByIdData = async () => {
   goods.value = res.result
 }
 
+//轮播图交互
+const currentIndex = ref(1)
+const onChange: UniHelper.SwiperOnChange = (e) => {
+  currentIndex.value = e.detail.current
+}
+
+//点击放大图片
+const onTapImage = (url: string) => {
+  uni.previewImage({
+    current: url,
+    urls: goods.value?.mainPictures,
+  })
+}
+
 onLoad(() => {
   getGoodsByIdData()
 })
@@ -28,15 +42,15 @@ onLoad(() => {
     <view class="goods">
       <!-- 商品主图 -->
       <view class="preview">
-        <swiper circular>
-          <swiper-item v-for="item in goods?.mainPictures" :key="item">
-            <image mode="aspectFill" :src="item" />
+        <swiper circular @change="onChange">
+          <swiper-item v-for="(item, index) in goods?.mainPictures" :key="item">
+            <image @tap="onTapImage" mode="aspectFill" :src="item" />
           </swiper-item>
         </swiper>
         <view class="indicator">
-          <text class="current">1</text>
+          <text class="current">{{ currentIndex + 1 }}</text>
           <text class="split">/</text>
-          <text class="total">5</text>
+          <text class="total">{{ goods?.mainPictures.length }}</text>
         </view>
       </view>
 
