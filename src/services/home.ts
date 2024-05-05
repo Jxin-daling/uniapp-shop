bc29e20f9d2d8d1ce44e17b5dacdf954787be534
Jxin_darling <3167044821@qq.com>
2024-05-05 13:01:18 +0800

    首页数据请求封装API接口

--- src/services/home.ts
+++ src/services/home.ts
@@ -1,5 +1,5 @@
 import { http } from "@/utils/https"
-import type { BannerItem } from "@/types/home"
+import type { BannerItem, CategoryItem,HotItem } from "@/types/home"
 
 export const getHomeBannerAPI = (distributionSite = 1) => {
     return http<BannerItem[]>({
@@ -8,5 +8,21 @@ export const getHomeBannerAPI = (distributionSite = 1) => {
         data: {
             distributionSite
         }
+    })
+}
+
+//首页分类接口
+export const getHomeCategoryAPI = () => {
+    return http<CategoryItem[]>({
+        method: "GET",
+        url: '/home/category/mutli'
+    })
+}
+
+//首页热门推荐接口
+export const getHomeHotAPI = () => {
+    return http<HotItem[]>({
+        method:"GET",
+        url:'/home/hot/mutli'
     })
 }
