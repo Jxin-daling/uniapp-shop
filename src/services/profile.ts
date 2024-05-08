0ef2c942f04c6036ffcbaa16d2879a3b2d564e14
Jxin_darling <3167044821@qq.com>
2024-05-08 17:22:25 +0800

    修改个人信息的请求接口

--- src/services/profile.ts
+++ src/services/profile.ts
@@ -1,9 +1,19 @@
-import type { ProfileDetail } from "@/types/member"
+import type { ProfileDetail, ProfileParams } from "@/types/member"
 import { http } from "@/utils/https"
 
 export const getMemberprofileAPI =()=>{
     return http<ProfileDetail>({
         method:"GET",
         url:'/member/profile'
+    })
+}
+
+
+//修改个人信息
+export const putMemberProfileAPI = (data: ProfileParams)=>{
+    return http<ProfileDetail>({
+        method:'PUT',
+        url:'/member/profile',
+        data
     })
 }
