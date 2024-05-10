eafa725ba9daa1174124283fd33a431318b5da0a
Jxin_darling <3167044821@qq.com>
2024-05-11 00:20:10 +0800

    购物车列表数据请求

---
+++ src/services/cart.ts
@@ -0,0 +1,17 @@
+import type { CartItem } from '@/types/cart'
+import { http } from '@/utils/https'
+
+export const postMemberCartAPI = (data: { skuId: string; count: number }) => {
+  return http({
+    method: 'POST',
+    url: '/member/cart',
+    data,
+  })
+}
+
+export const getMemberCartAPI = () => {
+  return http<CartItem[]>({
+    method: 'GET',
+    url: '/member/cart',
+  })
+}
